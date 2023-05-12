7969bcad506a8a6e8d793dcf56935804c1be87ec
DevPrinceK <princesamuelpks@gmail.com>
2023-05-12 15:29:48 +0000

    Create recently_container.dart

---
+++ dlcf/lib/screens/home/components/recently_container.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class RecentlyBox extends StatelessWidget {
+  // required params
+  final String title;
+  final String preacher;
+  final String url;
+  final String description;
+  final String thumbnailUrl;
+  const RecentlyBox({
+    Key? key,
+    required this.title,
+    required this.preacher,
+    required this.url,
+    required this.description,
+    required this.thumbnailUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final boxWidth = MediaQuery.of(context).size.width / 3;
+    return Container(
+      margin: const EdgeInsets.only(right: 10),
+      height: 120,
+      width: boxWidth,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(thumbnailUrl),
+          fit: BoxFit.cover,
+        ),
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
