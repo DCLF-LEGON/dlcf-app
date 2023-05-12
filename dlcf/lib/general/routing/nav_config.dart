36172c29d1e958d2a04c4767de44f83469924bf6
DevPrinceK <princesamuelpks@gmail.com>
2023-05-12 14:00:24 +0000

    Create nav_config.dart

---
+++ dlcf/lib/general/routing/nav_config.dart
@@ -0,0 +1,30 @@
+class RouteNames {
+  static const String index = 'index';
+  static const String home = 'home';
+  static const String onboarding = 'onboarding';
+  static const String signup = 'signup';
+  static const String login = 'login';
+  static const String error = 'error';
+  static const String verifyotp = 'verifyotp';
+  static const String otpverified = 'otpverified';
+  static const String resetpassword = 'resetpassword';
+  static const String resetpasswordsuccess = 'resetpasswordsuccess';
+  static const String discover = 'discover';
+  static const String notification = 'notification';
+  static const String profile = 'profile';
+  static const String notes = 'notes';
+  static const String video = 'video';
+  static const String channelmessages = 'channelmessages';
+  static const String give = 'give';
+  static const String givemomo = 'givemomo';
+  static const String givesuccess = 'givesuccess';
+  static const String aboutus = 'aboutus';
+  static const String doctrine = 'doctrine';
+  static const String doctrinedetail = 'doctrinedetail';
+}
+
+class Networks {
+  static const String mtn = 'MTN';
+  static const String voda = 'VODA';
+  static const String airteltigo = 'AIRTELTIGO';
+}
