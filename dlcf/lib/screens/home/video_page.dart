fb341743f8e8458b58b2f8ea1f2680bc3bdf9eee
DevPrinceK <princesamuelpks@gmail.com>
2023-05-12 15:31:46 +0000

    Create video_page.dart

---
+++ dlcf/lib/screens/home/video_page.dart
@@ -0,0 +1,53 @@
+import 'package:dlcf/general/components/bottom_nav.dart';
+import 'package:dlcf/screens/home/components/video_body.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class VideoScreen extends StatelessWidget {
+  final String title;
+  final String preacher;
+  final String url;
+  final String description;
+  final String thumbnailUrl;
+  const VideoScreen({
+    super.key,
+    required this.title,
+    required this.preacher,
+    required this.url,
+    required this.description,
+    required this.thumbnailUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.blue,
+          title: Text(title.toUpperCase()),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              GoRouter.of(context).pop();
+            },
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: VideoBody(
+              url: url,
+              description: description,
+              preacher: preacher,
+              thumbnailUrl: thumbnailUrl,
+              title: title,
+            ),
+          ),
+        ),
+        bottomNavigationBar: CustomBottomNav(selectedTab: 0),
+      ),
+    );
+  }
+}
