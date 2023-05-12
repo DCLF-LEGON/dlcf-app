36b04b00c346fa117fdfb83f5567d0280c7f47ec
DevPrinceK <princesamuelpks@gmail.com>
2023-05-12 15:37:55 +0000

    Create profile.dart

---
+++ dlcf/lib/screens/profile/profile.dart
@@ -0,0 +1,27 @@
+import 'package:dlcf/general/components/bottom_nav.dart';
+import 'package:dlcf/screens/profile/components/drawer_body.dart';
+// import 'package:dlcf/screens/profile/components/drawer_body2.dart';
+import 'package:dlcf/screens/profile/components/profile_body.dart';
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('PROFILE'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        drawer: const SafeArea(child: DrawerBody()),
+        body: const ProfileBody(),
+        bottomNavigationBar: CustomBottomNav(selectedTab: 4),
+      ),
+    );
+  }
+}
