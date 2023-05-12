4e54bd1d7527c2a05aa70d034fe9435e9d441440
DevPrinceK <princesamuelpks@gmail.com>
2023-05-12 15:38:42 +0000

    Create reset_password.dart

---
+++ dlcf/lib/screens/reset_password/reset_password.dart
@@ -0,0 +1,32 @@
+import 'package:dlcf/constants/colors.dart';
+import 'package:dlcf/screens/reset_password/components/body.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class ResetPassword extends StatelessWidget {
+  const ResetPassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: primaryColor,
+      ),
+      home: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios_outlined),
+            onPressed: () {
+              GoRouter.of(context).pop();
+            },
+          ),
+          elevation: 0,
+        ),
+        body: const Body(),
+      ),
+    );
+  }
+}
