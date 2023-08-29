d266a2289f1d01e6a270356a08e5c402784e1ab5
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-08-29 09:53:44 +0000

    Update momo_body.dart

--- dlcf/lib/screens/give/components/momo_body.dart
+++ dlcf/lib/screens/give/components/momo_body.dart
@@ -42,16 +42,6 @@ class _GiveMomoBodyState extends State<GiveMomoBody> {
     print(response.statusCode);
     print(response.reasonPhrase);
     return response.statusCode;
-    // Check the response status
-    // if (response.statusCode == 200) {
-    //   // Success, handle the response here if needed
-    //   print('Donation successful!');
-    //   return 200;
-    // } else {
-    //   // Error occurred, handle the error here if needed
-    //   print('Donation failed with status: ${response.statusCode}');
-    //   return response.statusCode;
-    // }
   }
 
   @override
